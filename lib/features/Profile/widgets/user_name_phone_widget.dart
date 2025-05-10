e843c59706bd769b72f367c7fef5a84b4a79215b
Mohamed Gamal <mohamedgamalfcis@gmail.com>
2025-05-10 06:54:45 +0300

    add name and phone in profile screen

--- lib/features/Profile/widgets/user_name_phone_widget.dart
+++ lib/features/Profile/widgets/user_name_phone_widget.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../core/storage/token_storage.dart';
 import '../../../core/theme/theme.dart';
 import 'package:provider/provider.dart';
-class UserNameAndPhoneNumber extends StatelessWidget {
+class UserNameAndPhoneNumber extends StatefulWidget {
   const UserNameAndPhoneNumber({super.key});
 
+  @override
+  State<UserNameAndPhoneNumber> createState() => _UserNameAndPhoneNumberState();
+}
+
+class _UserNameAndPhoneNumberState extends State<UserNameAndPhoneNumber> {
+
+  String _userName = '';
+  String _phoneNumber = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserName();
+  }
+  Future<void> _loadUserName() async {
+    final storage = TokenStorage();
+    final user = await storage.getUser();
+    if (mounted && user != null) {
+      setState(() {
+        _userName = "${user.firstName??""} ${user.secondName??""}";
+        _phoneNumber = user.phoneNumber??user.phone??"";
+      });
+    }
+  }
   @override
   Widget build(BuildContext context) {
     ThemeProvider themeProvider = Provider.of<ThemeProvider>(context);
@@ -19,7 +44,7 @@ class UserNameAndPhoneNumber extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            "Ahmed Salah",
+            _userName,
             style: TextStyle(
               fontSize: 16.sp,
             ),
@@ -28,7 +53,7 @@ class UserNameAndPhoneNumber extends StatelessWidget {
             height: 12.h,
           ),
           Text(
-            '+201098362091',
+            "+20 $_phoneNumber",
             style: TextStyle(
               color: themeProvider.currentThemeData!.primaryColor,
               fontSize: 16.sp,
