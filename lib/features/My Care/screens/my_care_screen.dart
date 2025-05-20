699e5a71b3d98ffc6655ca972bcc482db70b03ca
Mohamed Gamal <mohamedgamalfcis@gmail.com>
2025-05-20 10:28:43 +0300

    [WIP] Localization

--- lib/features/My Care/screens/my_care_screen.dart
+++ lib/features/My Care/screens/my_care_screen.dart
@@ -4,6 +4,7 @@ import 'package:shifa/core/theme/theme.dart';
 import 'package:shifa/core/widgtes/common_app_bar_child_theme.dart';
 import 'package:shifa/core/widgtes/watermark_widget.dart';
 import 'package:shifa/features/My%20Care/widgets/my_care_body.dart';
+import 'package:shifa/core/localization/app_extensions.dart';
 
 class MyCareScreen extends StatelessWidget {
   const MyCareScreen({super.key});
@@ -14,7 +15,7 @@ class MyCareScreen extends StatelessWidget {
     return WaterMark(
       backGroundColor: themeProvider.currentThemeData!.primaryColor,
       alignment: Alignment.center,
-      appBarChild: const CommonAppBarChildTheme(title: 'My Care'),
+      appBarChild: CommonAppBarChildTheme(title: context.tr.translate("my_care")),
       hasBorderRadius: true,
       contentChild: const Expanded(
         child: MyCareBody(),
