912ebb1d75e3a75ef3d865b92682b76070720302
Mohamed Gamal <mohamedgamalfcis@gmail.com>
2025-05-22 23:06:14 +0300

    [WIP] Localization

--- lib/core/widgtes/auth_appbar.dart
+++ lib/core/widgtes/auth_appbar.dart
@@ -7,7 +7,7 @@ class AuthAppbar extends StatefulWidget {
   final Widget body;
   final bool showBack;
 
-  const AuthAppbar({super.key, required this.body, this.showBack=true});
+  const AuthAppbar({super.key, required this.body, this.showBack = true});
 
   @override
   State<AuthAppbar> createState() => _AuthAppbarState();
@@ -16,32 +16,45 @@ class AuthAppbar extends StatefulWidget {
 class _AuthAppbarState extends State<AuthAppbar> {
   @override
   Widget build(BuildContext context) {
+    final locale = Localizations.localeOf(context);
+    final isArabic = locale.languageCode == 'ar';
     return Scaffold(
       appBar: AppBar(
         backgroundColor: AppTheme.onBoardingBG,
         leadingWidth: 100.w,
-        leading:widget.showBack? Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24.0.w),
-          child: InkWell(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child: Container(
-              height: 44.h,
-              width: 44.w,
-              decoration: BoxDecoration(
-                color: AppTheme.whiteColor,
-                border: Border.all(color: AppTheme.greyColor),
-                borderRadius: BorderRadius.circular(16.r),
-              ),
-              child: Image.asset(
-                Assets.arrowLeft,
-                width: 24.w,
-                height: 24.h,
-              ),
-            ),
-          ),
-        ):const SizedBox.shrink(),
+        leading: widget.showBack
+            ? Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0.w),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    height: 44.h,
+                    width: 44.w,
+                    decoration: BoxDecoration(
+                      color: AppTheme.whiteColor,
+                      border: Border.all(color: AppTheme.greyColor),
+                      borderRadius: BorderRadius.circular(16.r),
+                    ),
+                    child: isArabic
+                        ? Transform.scale(
+                            scaleX: -1,
+                            child: Image.asset(
+                              Assets.arrowLeft,
+                              width: 24.w,
+                              height: 24.h,
+                            ),
+                          )
+                        : Image.asset(
+                            Assets.arrowLeft,
+                            width: 24.w,
+                            height: 24.h,
+                          ),
+                  ),
+                ),
+              )
+            : const SizedBox.shrink(),
       ),
       backgroundColor: AppTheme.onBoardingBG,
       body: widget.body,
