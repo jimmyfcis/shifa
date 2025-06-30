a4e65c54212342e2af49025cdc0545e9358cbdb3
Mohamed Gamal <mohamedgamalfcis@gmail.com>
2025-06-30 19:54:54 +0300

    update news while changing theme

--- lib/core/widgtes/common_app_bar_child_theme.dart
+++ lib/core/widgtes/common_app_bar_child_theme.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
 import 'package:shifa/core/assets/svg/svg_assets.dart';
 import 'package:shifa/core/theme/theme.dart';
+import 'package:shifa/features/news/presentation/cubit/news_cubit.dart';
+
+import '../network/injection_container.dart';
 
 class CommonAppBarChildTheme extends StatelessWidget {
   final String? title;
@@ -15,19 +19,21 @@ class CommonAppBarChildTheme extends StatelessWidget {
   Widget build(BuildContext context) {
     ThemeProvider themeProvider = Provider.of<ThemeProvider>(context);
 
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 24.w),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: 32.h,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              themeProvider.currentTheme == ThemeEnum.shifa
-                  ? Row(
+    return BlocProvider(
+      create: (context) => sl<NewsCubit>(),
+      child: Builder(
+        builder: (context) {
+          return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 32.h),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    themeProvider.currentTheme == ThemeEnum.shifa
+                        ? Row(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         SvgPicture.asset(
@@ -44,7 +50,7 @@ class CommonAppBarChildTheme extends StatelessWidget {
                         )
                       ],
                     )
-                  : Row(
+                        : Row(
                       children: [
                         SvgPicture.asset(
                           SVGAssets.leksellHomeWhiteIcon,
@@ -65,33 +71,41 @@ class CommonAppBarChildTheme extends StatelessWidget {
                         ),
                       ],
                     ),
-              GestureDetector(
-                onTap: () {
-                  themeProvider.changeTheme(
-                      themeProvider.currentTheme == ThemeEnum.shifa
-                          ? ThemeEnum.leksell
-                          : ThemeEnum.shifa);
-                },
-                child: SvgPicture.asset(
-                  themeProvider.currentTheme != ThemeEnum.shifa
-                      ? SVGAssets.shifaHome
-                      : SVGAssets.leksellHome,
-                  width: 1.w,
-                  height: 36.h,
+                    GestureDetector(
+                      onTap: () {
+                        final newsCubit = context.read<NewsCubit>(); // safe here
+                        themeProvider.changeTheme(
+                          themeProvider.currentTheme == ThemeEnum.shifa
+                              ? ThemeEnum.leksell
+                              : ThemeEnum.shifa,
+                        );
+                        newsCubit.getNews(
+                          isLeksell: themeProvider.currentTheme == ThemeEnum.leksell,
+                        );
+                      },
+                      child: SvgPicture.asset(
+                        themeProvider.currentTheme != ThemeEnum.shifa
+                            ? SVGAssets.shifaHome
+                            : SVGAssets.leksellHome,
+                        width: 1.w,
+                        height: 36.h,
+                      ),
+                    )
+                  ],
                 ),
-              )
-            ],
-          ),
-          SizedBox(height: 8.h),
-          child ??
-              Text(
-                title ?? "",
-                style: TextStyle(
-                  fontSize: 24.sp,
-                  color: Colors.white,
-                ),
-              )
-        ],
+                SizedBox(height: 8.h),
+                child ??
+                    Text(
+                      title ?? "",
+                      style: TextStyle(
+                        fontSize: 24.sp,
+                        color: Colors.white,
+                      ),
+                    )
+              ],
+            ),
+          );
+        },
       ),
     );
   }
